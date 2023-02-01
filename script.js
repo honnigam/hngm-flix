75bb37a6cf473bbd702729851a9ea5d9094c5572
honnigam <lucasvm87@gmail.com>
2023-02-01 15:20:56 -0300

    update js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var filmeM = 'https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg'
-var filmeR = 'https://livrosfilmes2017.files.wordpress.com/2017/08/quatro-vidas-cachorro-4.jpg'
-var filmeL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-LHmK_WLUF8QvnsBL-WRXRByjzfGAE0KuOQ&usqp=CAU'
 
-document.write('<img src=' + filmeM + '>')
+var filmList = [] 
+
+filmList [0] = 'https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg'
+filmList [1] = 'https://livrosfilmes2017.files.wordpress.com/2017/08/quatro-vidas-cachorro-4.jpg'
+filmList [2] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-LHmK_WLUF8QvnsBL-WRXRByjzfGAE0KuOQ&usqp=CAU'
+
+for (var list=0; list<3; list++){
+   document.write('<img src=' + filmList[list] + '>')
+}
