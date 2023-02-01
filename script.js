4448ab288b860fdf8dadba114e53c0f8e1a45bde
honnigam <lucasvm87@gmail.com>
2023-02-01 14:08:40 -0300

    insert system

---
+++ script.js
@@ -0,0 +1,5 @@
+var filmeM = 'https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg'
+var filmeR = 'https://livrosfilmes2017.files.wordpress.com/2017/08/quatro-vidas-cachorro-4.jpg'
+var filmeL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-LHmK_WLUF8QvnsBL-WRXRByjzfGAE0KuOQ&usqp=CAU'
+
+document.write('<img src=' + filmeM + '>')
