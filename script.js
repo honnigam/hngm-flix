ab2bd639742294ccc9eb97dcb09367c3add36a35
honnigam <lucasvm87@gmail.com>
2023-02-02 15:13:29 -0300

    update js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-var filmList = [] 
+var filmList = ['https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg','https://br.web.img3.acsta.net/pictures/16/08/31/23/26/562395.png','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-LHmK_WLUF8QvnsBL-WRXRByjzfGAE0KuOQ&', 'https://pics.filmaffinity.com/Harry_Potter_and_the_Sorcerer_s_Stone-154820574-mmed.jpg','https://upload.wikimedia.org/wikipedia/pt/6/6f/Death_Race_poster.jpg' ]
 
-filmList [0] = 'https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg'
-filmList [1] = 'https://livrosfilmes2017.files.wordpress.com/2017/08/quatro-vidas-cachorro-4.jpg'
-filmList [2] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-LHmK_WLUF8QvnsBL-WRXRByjzfGAE0KuOQ&usqp=CAU'
+var filmName = ['Minha mãe é uma peça ', 'Quatro vidas de um cachorro ', 'Fast & furious ', 'Harry Potter ', 'Corrida mortal']
 
-for (var list=0; list<3; list++){
+for (var list=0; list<filmList.length; list++){
    document.write('<img src=' + filmList[list] + '>')
+}
+
+for (var list=0; list<filmName.length; list++){
+   document.write(filmName[list])
 }
